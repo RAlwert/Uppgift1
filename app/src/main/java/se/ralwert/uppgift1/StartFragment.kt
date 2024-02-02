6affd86da207857e0aa61f1ccb87d0816067ef29
Robin <ralwert@outlook.com>
2024-02-02 10:36:35 +0100

    uppgift 1 klar

---
+++ app/src/main/java/se/ralwert/uppgift1/StartFragment.kt
@@ -0,0 +1,45 @@
+package se.ralwert.uppgift1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import se.ralwert.uppgift1.databinding.FragmentStartBinding
+
+//
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private  val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+      //  return inflater.inflate(R.layout.fragment_start, container, false)
+        _binding = FragmentStartBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.Changetextbutton.setOnClickListener {
+            binding.WelcomeText.text = "Goodbye"
+        }
+    }
+}
